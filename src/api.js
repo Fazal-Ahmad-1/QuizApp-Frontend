8a9b596b81801909025fcce3186e16ca9b8ae3a1
Fazal Ahmad <fazalahmadofc@gmail.com>
2025-12-06 21:28:57 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE = "https://quizappbackend-1p04.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE,
